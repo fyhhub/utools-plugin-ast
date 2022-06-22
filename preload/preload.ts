3548baecdb442cdfd07eed760922b69cdf30a7bb
1131153523@qq.com <fanyihui@tuhu.cn>
2022-06-22 18:17:45 +0800

    feat: changes

--- preload/preload.ts
+++ preload/preload.ts
@@ -1,5 +1,7 @@
 
 utools.onPluginEnter(() => {
-  utools.setExpendHeight(700)
+  const display = utools.getPrimaryDisplay()
+  const height = display.size.height;
+  utools.setExpendHeight(height * (2 / 3))
 })
 export {}
